16efbbe1bb9eba5c0cab7da91efbe5fad7faaba0
Armita <farshinarmita@gmail.com>
2025-06-26 21:11:41 +0330

    Update LearningInCodes.py

--- French/LearningInCodes.py
+++ French/LearningInCodes.py
@@ -2,8 +2,11 @@ mots = {'se débrouiller' : 'to manage, to cope, to get by (especially in a tric
 # I add every new word i learn
 word = ' '
 chosen = ' '
-chosen = input("What do you want to work with? ")
-if chosen == "Dictionary":
-  word = input("what word are you looking for? ")
-  print(f'it means {mots[word]}')
+chosen = input("What do you want to work with? ").lower()
+if chosen == "dictionary":
+  word = input("What word are you looking for? ").lower()
+  if word in mots:
+    print(f'It means {mots[word]}')
+  else:
+    print("Oops, there is no word like that! Let's add it now!😊")
   
